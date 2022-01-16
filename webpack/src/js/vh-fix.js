d420f02f79c4506a2ac1339f2b360d25da3d7f2e
Saimon Kat <saimon.kat23@gmail.com>
2022-01-16 16:18:14 +0300

    webpack vh-fix update

--- webpack/src/js/vh-fix.js
+++ webpack/src/js/vh-fix.js
@@ -1,7 +1,8 @@
-// 100 vh fix
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', vh + 'px');
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', vh + 'px');
-});
+const appHeight = () => {
+    document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('DOMContentLoaded', appHeight)
+window.addEventListener('resize', appHeight)
+
+const mobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+mobileDevice && window.addEventListener('scroll', appHeight)
